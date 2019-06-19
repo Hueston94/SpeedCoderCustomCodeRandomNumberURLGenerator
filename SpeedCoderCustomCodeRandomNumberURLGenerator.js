03fbd3466c54bebb37a8ee94677006a91c9414bd
Hueston94 <30508835+Hueston94@users.noreply.github.com>
2019-06-19 23:59:21 +0100

    Add files via upload

---
+++ SpeedCoderCustomCodeRandomNumberURLGenerator.js
@@ -0,0 +1,8 @@
+var int1 = Math.floor(Math.random() * 58) + 1;
+var str1 = "http://www.speedcoder.net/lessons/customcode/";
+var str2 = int1.toString();
+var str3 = "/";
+var res = str1.concat(str2, str3);
+window.location.href = res;
+
+# Created Bookmarklet using https://mrcoles.com/bookmarklet/
